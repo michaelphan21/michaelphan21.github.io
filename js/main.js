8bd4ab17dd533d7b7b7ee4eeb8a2251731909151
Michael Phan <miphan@ucsd.edu>
2017-09-04 17:17:04 -0700

    added parallax ending, quote, and aboutme section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("#overlay, #overlay h1").each(function(i) {
+	$("#overlay, #overlay h1, #overlay_2, #overlay_2 h1").each(function(i) {
 		$(this).delay(i*2000).fadeIn(2000);
 	});
 	$(".navbar").hide().fadeIn(4000);
