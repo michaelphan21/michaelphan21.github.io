1f41f0cf15578e31d5b7254d84c3e1079881d3a4
Michael Phan <miphan@ucsd.edu>
2017-09-05 23:24:56 -0700

    Cleaned up stylesheet

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	//hide navbar first
 	$(".navbar").hide()
 	//fade in text in intro and end
-	$("#overlay, #welcome, .navbar, #overlay_2, #end").each(function(i) {
+	$("#overlay, #welcome, .navbar, #end").each(function(i) {
 		$(this).delay(i*2000).fadeIn(2000);
 	}); 	
 });
