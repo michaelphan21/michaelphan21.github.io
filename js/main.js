881464224af30ae482fe54e0b6ebdc226686d611
Michael Phan <miphan@ucsd.edu>
2017-09-05 20:54:03 -0700

    Added some text into aec section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
+	
+	//fade in text in intro and end
 	$("#overlay, #overlay h1, #overlay_2, #overlay_2 h1").each(function(i) {
 		$(this).delay(i*2000).fadeIn(2000);
 	});
+
+	//fade in nav bar
 	$(".navbar").hide().fadeIn(3000);
+
 });
