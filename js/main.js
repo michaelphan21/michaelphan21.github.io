0f69af4eebc3fe112188b41519c38be2e6e7cb26
Michael Phan <miphan@ucsd.edu>
2017-09-05 18:06:59 -0700

    Fixed photo for intro

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
 	$("#overlay, #overlay h1, #overlay_2, #overlay_2 h1").each(function(i) {
 		$(this).delay(i*2000).fadeIn(2000);
 	});
-	$(".navbar").hide().fadeIn(4000);
+	$(".navbar").hide().fadeIn(3000);
 });
