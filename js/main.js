eb95436adddb91de66498886869ccc0f4b5cf1e1
Michael Phan <michaelphan21@gmail.com>
2019-03-07 06:38:31 -0800

    deleted filed and modified index

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
-	//hide navbar first
-	$(".navbar").hide()
+
 	//fade in text in intro and end
 	$("#overlay, #welcome, .navbar, #end").each(function(i) {
-		$(this).delay(i*2000).fadeIn(2000);
+		$(this).delay(i).fadeIn(2000);
 	}); 	
 	
 	$(".navbar-nav li a").click(function(event) {
