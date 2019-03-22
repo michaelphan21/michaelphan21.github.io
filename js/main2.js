8fb62f311631dc59157911c5eb19654003126c50
Michael Phan <phami2@shcsd.sharp.com>
2019-03-22 10:33:25 -0700

    removed temp files

--- js/main2.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("ul.nav li a[href^='#']").click(function(){
-        $("html, body").stop().animate({
-            scrollTop: $($(this).attr("href")).offset().top
-        }, 400);
-    });
-});
